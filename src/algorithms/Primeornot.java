946fadca2fc19898a769bd5a43ef741314decbde
Namod <league@iMac-6.attlocal.net>
2019-03-04 16:52:13 -0800

    March 4, 2019

---
+++ src/algorithms/Primeornot.java
@@ -0,0 +1,25 @@
+package algorithms;
+
+import javax.swing.JOptionPane;
+
+public class Primeornot {
+public static void main(String[] args) {
+	String name=JOptionPane.showInputDialog("Dear user, please enter your name into the box below!");
+	String number = JOptionPane.showInputDialog("Okay, " +name+ ". Please enter a number into the box below. I will then tell you wether the number is prime or not. Ready? Go! ");
+int x = Integer.parseInt(number);
+	
+	System.out.println(x);
+	for (int s=2; s<x; s++) {
+if (x % s==0) {
+	JOptionPane.showMessageDialog(null, "This number is not prime.");
+	System.exit(0);
+}
+
+	}
+	
+	
+	
+	
+	
+}
+}
